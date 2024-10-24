f4e8c6a089d65f1f5b0645666c7fc49104a84ff2
abi daffa <abibelajar1@gmail.com>
2024-10-24 22:38:31 +0700

    First Commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+print("Hello World") #output hello world
+print(1 + 2) #output 3
+print("Hello", 4) #output Hello 4
+
+print('Hello' * 3) #output HelloHelloHello
+print(10 * 20) #output 200
+
+print('Hello' + 'World') #output HelloWorld
+print(10 + 20) #output 30
+
+print('Hello' + 3) # akan muncul error
+
